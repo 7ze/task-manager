d8ce6321da71c3f24911ff6d204fdecbbdc5d021
The_philosopher <shinudonney@tuta.io>
2021-03-08 13:02:07 +0530

    Add simple Error Handling Exceptions

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './tasks.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -40,7 +40,11 @@ export class TasksService {
   }
 
   getTaskById(id: string): Task {
-    return this.tasks.find((task) => task.id === id);
+    const found = this.tasks.find((task) => task.id === id);
+    if (!found) {
+      throw new NotFoundException(`Task with id '${id}' not found.`);
+    }
+    return found;
   }
 
   updateTask(id: string, status: TaskStatus): Task {
@@ -50,6 +54,7 @@ export class TasksService {
   }
 
   deleteTask(id: string): void {
-    this.tasks = this.tasks.filter((task) => task.id !== id);
+    const found = this.getTaskById(id);
+    this.tasks.splice(this.tasks.indexOf(found), 1);
   }
 }
