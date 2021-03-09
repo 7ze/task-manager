78fcf465bcc85154667ac995b2c8d59256c64209
The_philosopher <shinudonney@tuta.io>
2021-03-09 19:40:38 +0530

    Rename resource

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,5 +1,5 @@
 import { IsIn, IsNotEmpty, IsOptional } from 'class-validator';
-import { TaskStatus } from '../tasks.model';
+import { TaskStatus } from '../task.model';
 
 export class GetTasksFilterDto {
   @IsOptional()
