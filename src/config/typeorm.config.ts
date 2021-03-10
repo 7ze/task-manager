85b308ad3da14ed62551fec23e7c1929a7fabab7
The_philosopher <shinudonney@tuta.io>
2021-03-10 10:13:09 +0530

    Fix a connection bug

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -7,6 +7,6 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   username: 'postgres',
   password: 'postgres',
   database: 'taskmanager',
-  entities: [__dirname + '/../**/*.entity.ts'],
+  entities: [__dirname + '/../**/*.entity.{js, ts}'],
   synchronize: true,
 };
