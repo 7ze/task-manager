4853d4c2c08c32e7959d6726f9dac04255334786
The_philosopher <shinudonney@tuta.io>
2021-03-14 12:41:34 +0530

    Implement auth middleware in tasks controller

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -9,9 +9,11 @@ import {
   Patch,
   Post,
   Query,
+  UseGuards,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
@@ -20,6 +22,7 @@ import { Task } from './task.entity';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
+@UseGuards(AuthGuard())
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
