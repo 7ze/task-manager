0fc0c186ff3f9977dfd22213659133b1ae048c85
The_philosopher <shinudonney@tuta.io>
2021-03-18 19:47:36 +0530

    Add config files

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,17 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as config from 'config';
 
 async function bootstrap() {
+  const port: number = config.get('server.port');
   const logger = new Logger('⚡ Server');
-
   const app = await NestFactory.create(AppModule);
-  const PORT = process.env.PORT ?? 3000;
+  const PORT = process.env.PORT ?? port;
+
   await app.listen(PORT);
 
   logger.log(`alive on http://localhost:${PORT}`);
 }
+
 bootstrap();
